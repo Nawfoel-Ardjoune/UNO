90978755c56edbcc8462620bcd715c6177904b00
Ludivine De Sa <47360167+pouplesse@users.noreply.github.com>
2020-02-03 23:31:44 +0100

    Update Uno.h

--- Uno.h
+++ Uno.h
@@ -1,21 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <time.h>
+#include <stdbool.h>
 
-struct Carte{// les cartes ont une valeur, une couleur, un type et leurs nombre
+typedef Carte Carte_t;
+struct Carte{// les cartes ont une valeur, une couleur, un type et leurs nombres
 	int nbr;
 	int couleur;
 	int valeur;
 	bool spe;
-};
-typedef Carte Carte_t;
-
- struct Pile{
-	Carte_t carte;
 	Carte_t *suivante;
+};
+
+typedef Pile Pile;
+ struct Pile{
+	Carte_t *premiereCarte;
 	int top = -1;
 };
-typedef Pile Pile_t;
+
+typedef struct Joueur Joueur_t;
+struct Joueur{
+	Pile main;
+	int score;
+};
+
+
+int initialisationPaquet();
+void Melange();
+void empiler();
 
 void push(Pile_t pile, Carte_t carte);
 Carte_t pop(Pile_t pile);
